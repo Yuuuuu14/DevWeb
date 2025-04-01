08c7b69e69964340ba1719715363ff45a3483157
Yu <yl11519@nyu.edu>
2025-04-01 09:53:28 -0400

    initial commit

---
+++ week10/inclass/exercise.js
@@ -0,0 +1,8 @@
+document.getElementById("alertButton").onclick = function () {
+    let name = "Hi"
+    window.alert("nobody can help you");
+};
+function demo(){
+document.getElementById("demo"). innerHTML = "Hello World!";
+}
+console.log("Helo World!");
---
+++ week10/inclass/main.js
@@ -0,0 +1,22 @@
+var x = 5;
+var y = 6.738102;
+var hi = "hi";
+var u = x + y;
+
+document.getElementById("alertButton").onclick = function () {
+    let name = "Hi"
+    window.alert("hi");
+};
+console.log("Helo World!");
+let carName = "Volvo";
+let year, color, model;
+year = 2020;
+color = "red";
+model = "X5";
+document.getElementById("car").innerHTML = carName + "" + "was my first car." + "It was a " + "color" + "model";
+
+let name = "Yu"
+let age = "20";
+let coding = true;
+
+document.getElementById("demo").innerHTML = "Hello,my name is " + name + "and I am" + age + "years old" + "I am a coder" + coding;
